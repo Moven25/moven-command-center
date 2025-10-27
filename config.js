5ae80595caf7978a4e2c18966fcd2441ff3e48fd
Michael Munroe <michaelmunroe@Michaels-Mac-mini.local>
2025-10-27 14:33:13 -0400

    Initial MOVEN Command Center commit

---
+++ config.js
@@ -0,0 +1,17 @@
+window.MOVEN_CONFIG = {
+  ACCESS_CODE: "Moven2026",
+  SHEET_PUBLISH_ID: "2PACX-1vS7NYEayV6hx1prr66ZMfNn7mWruqNKDVyvPCwNhkvKvnzQ-4zCLTSkRQtv9QC7geaBqSYISokNBPMv",
+  TABS: {
+    market_power: "0",
+    seasonal_flow: "1413461949",
+    lane_command: "1012805009",
+    fleet_checkin: "163017449",
+    fuel_zones: "33721459"
+  },
+  ALERT_THRESHOLDS: {
+    market_delta_pts: 10,
+    fuel_delta_usd: 0.15,
+    idle_hours: 24,
+    nws_levels: ["Advisory", "Watch", "Warning"]
+  }
+};
