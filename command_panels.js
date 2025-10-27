fe7349e2fa9ab335c9ace02f2a2f86f189d2ba00
Moven25 <michael.munroe16@gmail.com>
2025-10-27 16:25:29 -0400

    Create command_panels.js

---
+++ command_panels.js
@@ -0,0 +1,9 @@
+// MOVEN Command Panels (Phase 2 Core Framework)
+function loadPanel(panelId) {
+  const panels = document.querySelectorAll(".panel-content");
+  panels.forEach(p => p.style.display = "none");
+  document.getElementById(panelId).style.display = "block";
+}
+
+// Default open Mission Control
+window.onload = () => loadPanel("missionControl");
