a11c692a15fa356491bb8d57fc3eede21a23c9b9
Moven25 <michael.munroe16@gmail.com>
2025-10-28 10:11:06 -0400

    Delete command_panels.js

--- command_panels.js
+++
@@ -1,9 +0,0 @@
-// MOVEN Command Panels (Phase 2 Core Framework)
-function loadPanel(panelId) {
-  const panels = document.querySelectorAll(".panel-content");
-  panels.forEach(p => p.style.display = "none");
-  document.getElementById(panelId).style.display = "block";
-}
-
-// Default open Mission Control
-window.onload = () => loadPanel("missionControl");
