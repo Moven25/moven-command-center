cbe279f9295edcbd12303d37eaec68eaf0692403
Michael Munroe <michaelmunroe@Michaels-Mac-mini.local>
2025-12-24 21:44:50 -0500

    Wrap dashboard in AppShell layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard.jsx";
+import AppShell from "./components/AppShell";
+import Dashboard from "./pages/Dashboard";
 
-export default function App() {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <AppShell>
+      <Dashboard />
+    </AppShell>
   );
 }
+
+export default App;
